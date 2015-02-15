e887e7a6ec4c7df26720e1f683b977f31c7798e8
Antti Stenvall <antti@stenvall.fi>
2015-02-15 08:38:42 +0200

    Modified sql settings in armo

--- armo.php
+++ armo.php
@@ -4,15 +4,21 @@ namespace armo;
 
 class armo {
 
+  // use also sql
   const databaseName = 'mcc_irc';
   const users = 'users';
   const messages = 'quotes';
+  const extendToSql = true;
 
   private $data;
+  
+  private $isStatic;
 
   public function __construct($user) {
     $fileName = __DIR__ . '/quotes/' . $user . '.json';
-    if (!file_exists($fileName)) {
+    if (!file_exists($fileName) && !self::extendToSql) {
+      throw new \Exception("Citations for '$user' do not exist.", 310);
+    } elseif (!file_exists($fileName)) {
       // try from database
       $db = self::getDB();
       $data = $db->getById(self::users, $user, 'username', 'id');
@@ -20,18 +26,20 @@ class armo {
         throw new \Exception("Citations for '$user' do not exist.", 310);
       }
       // now fetch quotes
-      $query = $db->prepare('select message from ' . self::messages . ' where userId=:userId order by timestamp desc');
+      $query = $db->prepare('select message, UNIX_TIMESTAMP(timestamp) as timestamp from ' . self::messages . ' where userId=:userId order by timestamp desc');
       $query->bindValue(':userId', $data['id'], \PDO::PARAM_INT);
-      $this->data = $db->getDataList($query);
+      $this->data = $db->getData($query);
+      $this->isStatic = false;
       if (count($this->data) == 0) {
         throw new \Exception("Citations for '$user' do not exist.", 310);
       }
     } else {
       $this->data = json_decode(file_get_contents($fileName), true);
+      $this->isStatic = true;
     }
   }
 
-  // static methods
+  // static methods doesn't include now sql
   static public function available($getAmounts = false) {
     $who = glob(__DIR__ . '/quotes/*.json');
     $av = array();
@@ -47,36 +55,41 @@ class armo {
   }
 
   static function save($username, $quote) {
-    // This is about saving to database
-    $db = self::getDB();
-    $data = $db->getById(self::users, $username, 'username', 'id');
-    if (!isset($data['id'])) {
-      $db->insert(self::users, array('username' => $username));
-      $userId = $db->getLastInsertId();
-    } else {
-      $userId = $data['id'];
-    }
-    $db->insert(self::messages, array('userId' => $userId,
-        'message' => $quote));
-    // OLD METHOD
-    /*
-      $fileName = __DIR__ . '/quotes/' . $user . '.json';
-      if (!file_exists($fileName)) {
-      $data = array($quote);
+    if (self::extendToSql) {
+      // This is about saving to database
+      $db = self::getDB();
+      $data = $db->getById(self::users, $username, 'username', 'id');
+      if (!isset($data['id'])) {
+        $db->insert(self::users, array('username' => $username));
+        $userId = $db->getLastInsertId();
       } else {
-      $data = json_decode(file_get_contents($fileName), true);
-      array_push($data, $quote);
+        $userId = $data['id'];
+      }
+      $db->insert(self::messages, array('userId' => $userId,
+          'message' => $quote));
+    } else {
+      $fileName = __DIR__ . '/quotes/' . $username . '.json';
+      if (!file_exists($fileName)) {
+        $data = array($quote);
+      } else {
+        $data = json_decode(file_get_contents($fileName), true);
+        array_push($data, $quote);
       }
       file_put_contents($fileName, json_encode($data, JSON_PRETTY_PRINT));
-     */
+    }
   }
 
   // public methods
   public function get($ind = null) {
     if (is_null($ind) || $ind > $this->getMaxInd()) {
-      return $this->data[rand(0, $this->getMaxInd())];
+      $ind = rand(0, $this->getMaxInd());      
     }
-    return $this->data[$ind];
+    if ($this->isStatic){
+      return $this->data[$ind];
+    }
+    else {
+      return '"' . $this->data[$ind]['message'] . '" ' . date('D d.m.Y H:i:s',$this->data[$ind]['timestamp']);
+    }
   }
 
   public function getMaxInd() {
